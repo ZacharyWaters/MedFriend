4d6d9070854fa3112e91afda466efc15a327e9c7
apexhydra <33587025+apexhydra@users.noreply.github.com>
2019-10-03 17:53:58 -0400

    Initial Upload

---
+++ app/src/main/java/com/example/medfriend/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.medfriend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        int X = 2;
+        setContentView(R.layout.activity_main);
+    }
+}
