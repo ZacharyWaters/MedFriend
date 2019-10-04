266baa368b260f9ec2d07d4736d9ab3acd9874df
apexhydra <zachwaterssteam@yahoo.com>
2019-10-04 03:38:35 -0400

    moreLoginUpdates

--- app/src/main/java/com/example/medfriend/Login.java
+++ app/src/main/java/com/example/medfriend/Login.java
@@ -10,7 +10,7 @@ import android.widget.ImageButton;
 
 public class Login extends AppCompatActivity {
 
-    Button registerButton;
+    ImageButton registerButton;
     View.OnClickListener registerListener = new View.OnClickListener(){
         public void  onClick  (View  v){
             startActivity(new Intent(Login.this, Register.class));
@@ -24,16 +24,28 @@ public class Login extends AppCompatActivity {
         }
     };
 
+    ImageButton forgottenButton;
+    View.OnClickListener forgottenListener = new View.OnClickListener(){
+        public void  onClick  (View  v){
+            startActivity(new Intent(Login.this, Homepage.class));
+        }
+    };
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        registerButton = (Button) findViewById(R.id.registerButton);
+        registerButton = (ImageButton)findViewById(R.id.registerIbutton);
         registerButton.setOnClickListener(registerListener);
+
         loginButton = (Button) findViewById(R.id.loginButton);
         loginButton.setOnClickListener(loginListener);
+
+        forgottenButton = (ImageButton) findViewById(R.id.forgotIbutton);
+        loginButton.setOnClickListener(forgottenListener);
 
     }
 }
