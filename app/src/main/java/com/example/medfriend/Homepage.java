28c35dfc5a8394147a7a42d2aeee300a3644e1fd
apexhydra <zachwaterssteam@yahoo.com>
2019-10-04 03:54:21 -0400

    more connectios

--- app/src/main/java/com/example/medfriend/Homepage.java
+++ app/src/main/java/com/example/medfriend/Homepage.java
@@ -2,13 +2,26 @@ package com.example.medfriend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Homepage extends AppCompatActivity {
+
+    Button addDoseButton;
+    View.OnClickListener addDoseListener = new View.OnClickListener(){
+        public void  onClick  (View  v){
+            startActivity(new Intent(Homepage.this, AlarmManager.class));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        addDoseButton = (Button) findViewById(R.id.addDoseButton);
+        addDoseButton.setOnClickListener(addDoseListener);
     }
 }
