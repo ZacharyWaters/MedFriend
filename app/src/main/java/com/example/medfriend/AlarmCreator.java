c9e81e4d7087e9c27a89c47bbdbb6b18678823f8
apexhydra <zachwaterssteam@yahoo.com>
2019-10-04 16:16:42 -0400

    removed some of the failed alarm stuff

--- app/src/main/java/com/example/medfriend/AlarmCreator.java
+++ app/src/main/java/com/example/medfriend/AlarmCreator.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -11,40 +12,11 @@ import android.content.Context;
 
 import java.text.DateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AlarmCreator extends AppCompatActivity {
 
     TextView timerText;
-
-    public void onTimeSet(int hourOfDay, int minute) {
-        Calendar c = Calendar.getInstance();
-        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
-        c.set(Calendar.MINUTE, minute);
-        c.set(Calendar.SECOND, 0);
-
-        updateTimeText(c);
-        startAlarm(c);
-    }
-
-    private void updateTimeText(Calendar c) {
-        String timeText = "Alarm set for: ";
-        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
-
-        //mTextView.setText(timeText);
-    }
-
-    private void startAlarm(Calendar c) {
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        Intent intent = new Intent(this, AlertReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
-
-        if (c.before(Calendar.getInstance())) {
-            c.add(Calendar.DATE, 1);
-        }
-
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
-    }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
