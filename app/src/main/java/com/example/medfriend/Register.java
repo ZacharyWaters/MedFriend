db9bffb6011f59618fb5fdad7c4bb9c81511f3fb
apexhydra <zachwaterssteam@yahoo.com>
2019-10-06 03:15:36 -0400

    added some database testing

--- app/src/main/java/com/example/medfriend/Register.java
+++ app/src/main/java/com/example/medfriend/Register.java
@@ -3,12 +3,22 @@ package com.example.medfriend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Register extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("Users&Scores");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        String databaseEntry = "Test";
+        myRef.push().child("User|Score").setValue(databaseEntry);
     }
 }
