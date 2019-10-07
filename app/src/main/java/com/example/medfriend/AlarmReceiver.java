9487f9bce5378832d2545e3da3392cf5d3f2f329
cmathis35 <cmmathis35@gatech.edu>
2019-10-06 22:28:03 -0400

    added the ability to turn off the alarm and an activity that pops up when the alarm goes off

--- app/src/main/java/com/example/medfriend/AlarmReceiver.java
+++ app/src/main/java/com/example/medfriend/AlarmReceiver.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver
@@ -14,12 +16,26 @@ public class AlarmReceiver extends BroadcastReceiver
     public void onReceive(Context context, Intent intent)
     {
         Toast.makeText(context, "Alarm! Wake up! Wake up!", Toast.LENGTH_LONG).show();
-        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
-        if (alarmUri == null)
-        {
-            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+//        if (alarmUri == null)
+//        {
+//            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//        }
+//        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+//        ringtone.play();
+
+        try {
+            Bundle bundle = intent.getExtras();
+            String message = bundle.getString("alarm_message");
+
+            Intent newIntent = new Intent(context, PopupActivity.class);
+            newIntent.putExtra("alarm_message", message);
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            context.startActivity(newIntent);
+        } catch (Exception e) {
+            e.printStackTrace();
+
         }
-        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
-        ringtone.play();
     }
 }
---
+++ app/src/main/java/com/example/medfriend/PopupActivity.java
@@ -0,0 +1,45 @@
+package com.example.medfriend;
+
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.Button;
+
+public class PopupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_popup);
+
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null)
+        {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        final Ringtone ringtone = RingtoneManager.getRingtone(getApplicationContext(), alarmUri);
+        ringtone.play();
+
+
+        Button fab = findViewById(R.id.stopbutton);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ringtone.stop();
+                Intent newIntent = new Intent(getApplicationContext(), Homepage.class);
+                getApplicationContext().startActivity(newIntent);
+            }
+        });
+    }
+
+}
